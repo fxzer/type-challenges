8181b77790f346916093fd57838304f633df6e88
fxzer <wy3010099292@163.com>
2023-05-14 18:23:11 +0800

    feat: completed some challenges

---
+++ 04471-medium-zip.ts
@@ -0,0 +1,16 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Zip<[], []>, []>>,
+  Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
+  Expect<Equal<Zip<[1, 2, 3], ['1', '2']>, [[1, '1'], [2, '2']]>>,
+  Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
+  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>,
+]
+
+
+// ============= Your Code Here =============
+type Zip<A extends any[], B extends any[], L extends any[] = []> = L['length'] extends A['length'] | B['length']
+  ? L
+  : Zip<A, B, [...L, [A[L['length']], B[L['length']]]]>
