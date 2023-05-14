573bac3b60fa5346780fe24eb43c72a13ff21068
fxzer <wy3010099292@163.com>
2023-05-14 17:48:22 +0800

    feat: completed some challenges

---
+++ 02257-medium-minusone.ts
@@ -0,0 +1,21 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<MinusOne<1>, 0>>,
+  Expect<Equal<MinusOne<55>, 54>>,
+  Expect<Equal<MinusOne<3>, 2>>,
+  Expect<Equal<MinusOne<100>, 99>>,
+  Expect<Equal<MinusOne<1101>, 1100>>,
+  Expect<Equal<MinusOne<0>, -1>>,
+  Expect<Equal<MinusOne<9_007_199_254_740_992>, 9_007_199_254_740_991>>,
+]
+
+
+// ============= Your Code Here =============
+
+type Pop<T extends any[]> = T extends [...infer head, any] ? head : never; 
+
+type MinusOne<T extends number, A extends any[] = []> = A['length'] extends T
+  ? Pop<A>['length']
+  : MinusOne<T, [...A, 0]>
